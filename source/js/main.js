66167b40ca402c094bec2375fe257de5e6178145
Denis FL <delchyve@gmail.com>
2013-07-30 14:14:26 +0400

    Initial commit

---
+++ source/js/main.js
@@ -0,0 +1,17 @@
+//= require vendor/jquery.min.js
+//= require plugins.js
+
+var App;
+
+$(function () {
+    'use strict';
+    App = {
+        init: function () {
+
+        },
+        cacheElements: function () {
+
+        }
+    };
+    App.init();
+});
